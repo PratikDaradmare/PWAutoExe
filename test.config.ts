cf603896158e6ce21cc46ed07d7d135bb3c31812
Pratik Daradmare <pratik.darad123@gmail.com>
2026-07-16 13:44:47 +0530

    payment page done

--- test.config.ts
+++ test.config.ts
@@ -12,4 +12,11 @@ export class TestConfig{
     productName="MacBook"
     productQuantity="2"
     totalPrice="$1,204.00"
+
+    //payment details
+    nameonCard="pratik darad"
+    cardNumber="123456789012"
+    cvc="123"
+    expirationmonth="12"
+    expirationyear="2029"
 }
